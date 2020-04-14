bb69393c572a96379f3d4c092761ce1b0da2a3e3
Bart Wijnants <bart.w@sparkcentral.com>
2020-04-14 15:11:06 +0200

    replace readme + create history

--- index.js
+++ index.js
@@ -95,6 +95,28 @@ const clearGatsbyConfig = () =>
       writeFile("gatsby-config.js", gatsbyConfigData)
     );
 
+const replaceReadme = () =>
+  writeFile(
+    "README.md",
+    `# ${name}
+
+This repo was initialized from https://github.com/bartw/gatsby-tailwind-starter using https://github.com/bartw/karapu.
+
+## Quickstart
+
+\`\`\`shell
+npm install
+npm start
+\`\`\`
+
+## License
+
+This repo is licensed under the [MIT License](LICENSE).
+`
+  );
+
+const createHistory = () => Git.Repository.init(".", 0);
+
 Git.Clone("https://github.com/bartw/gatsby-tailwind-starter", name)
   .then(() => {
     process.chdir(name);
@@ -102,6 +124,8 @@ Git.Clone("https://github.com/bartw/gatsby-tailwind-starter", name)
   .then(eraseHistory)
   .then(clearPackage)
   .then(clearGatsbyConfig)
+  .then(replaceReadme)
+  .then(createHistory)
   .catch((error) => {
     console.log(error);
   });
